84408eacbc972ed408122870ca5d3d61bdb637c0
malgorzatasojka-sid <malgorzata.sojka@edu.uekat.pl>
2025-12-13 13:43:15 +0100

    Fix PEP8 compliance

--- zad_1_operatory.py
+++ zad_1_operatory.py
@@ -1,5 +1,6 @@
 def przywitanie(name: str, surname: str):
     return f"Cześć {name} {surname}!"
 
+
 wynik = przywitanie("Gosia", "Sojka")
 print(wynik)
--- zad_2.py
+++ zad_2.py
@@ -1,5 +1,6 @@
 def multiply(a: int, b: int) -> int:
     return a * b
+
+
 result = multiply(3, 7)
 print(result)
-
